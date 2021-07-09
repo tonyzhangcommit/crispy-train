b6801e2a04daef3c58117ed5333393096edf7c3c
= <anyou@mpad365.com>
2021-07-09 14:05:18 +0800

    选择排序

--- data_structure/Quick_sort.py
+++ data_structure/Quick_sort.py
@@ -5,8 +5,29 @@
 # @File:Quick_sort.py
 
 # 快速排序python代码实现
+import time
+import random
+
+# 自定义装饰器
+def timer_count(f):
+    def decor(*args,**kwargs):
+        s_time = time.time()
+        r = f(*args,**kwargs)
+        e_time = time.time()
+        print("{}函数耗时{}秒".format(f.__name__,e_time-s_time))
+        return r
+    return decor
 '''
     快速排序思路：
-        递归分治思想
+        递归分治思想，每一轮循环都将待排序列表以某个值进行分割，然后依次递归处理
         实现代码如下：
 '''
+
+def quick_sort(l,start,end):
+    i = start
+    j = end
+    # 凡是涉及到递归操作的函数，必须存在一个终止条件
+    if i > j:
+        return
+    while i < j:
+        pass
--- data_structure/select_sort.py
+++ data_structure/select_sort.py
@@ -3,9 +3,37 @@
 # @Time:2021/7/6 10:59
 # @Author:ZhangFY
 # @File:select_sort.py
+
 import random
+import time
+
 '''
     选择排序 python 实现方法
+    算法核心，类似于冒泡排序算法，每次从无序数据中选出一个最大（小）值，插入到前方有序队列中去
 '''
+# 自定义装饰器
+def timer_count(f):
+    def decor(*args,**kwargs):
+        s_time = time.time()
+        r = f(*args,**kwargs)
+        e_time = time.time()
+        print("{}函数耗时{}秒".format(f.__name__,e_time-s_time))
+        return r
+    return decor
 
-l = [random.randint(0,30) for x in range(20)]
+def select_way1(l):
+    print('排序前:',l)
+    for i in range(len(l)-1):
+        min = l[i]       # 每一轮循环的最小值
+        index = i
+        for j in range(i,len(l)):
+            if l[j] < min:
+                min = l[j]
+                index = j
+        l[i],l[index] = l[index],l[i]
+    print("排序后:",l)
+
+
+if __name__ == '__main__':
+    l = [random.randint(10,100) for x in range(10)]
+    select_way1(l)
