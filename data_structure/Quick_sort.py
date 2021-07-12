c8b980fd45ebdb80b83c9aa8bc2ac97fae416c81
= <anyou@mpad365.com>
2021-07-12 08:58:41 +0800

    修复快速排序算法

--- data_structure/Quick_sort.py
+++ data_structure/Quick_sort.py
@@ -27,9 +27,9 @@ def quick_sort(l,start,end):
     i = start
     j = end
     # 凡是涉及到递归操作的函数，必须存在一个终止条件
-    if i >= j:
+    if i > j:
         return
-    stand = l[i]      # 基准数值，一轮循环后以这个数为基准分为两部分，前方都比它小，后方都比它大
+    stand = l[start]      # 基准数值，一轮循环后以这个数为基准分为两部分，前方都比它小，后方都比它大
     while i < j:
         while i < j and l[j] >= stand:
             j -= 1
@@ -41,14 +41,13 @@ def quick_sort(l,start,end):
         # 这个小循环从前往后找，找到第一个比stand大的数，停止循环，输出当前索引值
         l[j] = l[i]
         # 上一步赋值导致的列表中存在两个l[j]值，这一步赋值将后方的l[j]替换为l[i],最终列表中存在两个l[i]
-
     l[i] = stand
     quick_sort(l,start,i-1)
     quick_sort(l,i+1,end)
 
 if __name__ == '__main__':
-    l = [random.randint(10,99) for x in range(60000)]
-    # ----------------------------本机测试超6万条数据就会出现栈溢出错误，待优化-----------------------------------------
+    l = [random.randint(10,99) for x in range(1000)]
+    # ----------------------------本机测试超100万条数据就会出现栈溢出错误，待优化-----------------------------------------
     print("排序前:",l)
     s_time = time.time()
     quick_sort(l,0,len(l)-1)
