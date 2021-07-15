a43d87e59259e2e03c4722f1de93e5f2a9962c19
= <anyou@mpad365.com>
2021-07-15 19:51:15 +0800

    堆排序核心功能编写

--- data_structure/heap_sort.py
+++ data_structure/heap_sort.py
@@ -6,6 +6,8 @@
 
 # 堆排序
 # 堆排序为一种选择排序，最坏，最好，平均复杂度都是O(nlogn),不稳定排序
+# 算法核心，从最后的子树开始递归，使得父节点的值最大，最后将首尾元素调换位置
+# 算法核心类似希尔排序，只是分组形式不同，思想相同
 
 import random
 import time
@@ -20,10 +22,24 @@ def timing(func):
         return ref
     return deact
 
+# 算法核心为递归待排序数据构建的完全二叉树的每个子树，使得每个父节点值为最大
 def heap_sort(l):
-    print('排序前：',l)
+    # 根据完全二叉树性质，得出所有父节点对应的待排序的数据的索引
+    # 最后一个父节点索引为 len(l) // 2 - 1 ，所有父节点列表为 0 到 len(l) // 2 - 1
+    # 父节点两个子节点索引值（如果存在的话）左子节点 2*i + 1,右子节点 2*i + 2 其中i 为父节点
+    # 每一轮比较涉及所有树的的比较
+    last_p_index = len(l) // 2 - 1
+    while last_p_index >= 0:
+        if 2 * last_p_index + 2 < len(l) -1:  # 证明最后一个非子叶节点存在右孩子,此节点必定存在左孩子
+            if l[2 * last_p_index + 2] > l[last_p_index]:
+                l[2*last_p_index+2],l[last_p_index] = l[last_p_index],l[2*last_p_index+2]   # 交换右孩子
+        if l[2 * last_p_index + 1] > l[last_p_index]:
+            l[2 * last_p_index + 1], l[last_p_index] = l[last_p_index], l[2 * last_p_index + 1]  # 交换左孩子
+        last_p_index -= 1
 
 
 if __name__ == '__main__':
     l = [random.randint(5,100) for x in range(30)]
+    print(l)
     heap_sort(l)
+    print(l)
