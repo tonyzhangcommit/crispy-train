b4e9f46069206c8b07441534bf8dd78401ed8751
= <anyou@mpad365.com>
2021-07-22 19:25:39 +0800

    顺序查找和折半查找

---
+++ data_structure/search_algorithm_order.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*-coding:utf-8-*-
+# @Time:2021/7/22 19:01
+# @Author:ZhangFY
+# @File:search_algorithm_order.py
+
+
+'''
+    顺序查找和折半查找 查找方式的实现
+    
+'''
+import time
+import random
+
